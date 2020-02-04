f61ffb61275d432dd43c37360562c26e82346bc0
Толянчик <tolyara1961@gmail.com>
2020-02-04 11:27:14 +0200

    sort using collections

--- src/main/java/com/apps/swing_app_1/SinglePageApplication.java
+++ src/main/java/com/apps/swing_app_1/SinglePageApplication.java
@@ -1,16 +1,19 @@
 package com.apps.swing_app_1;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
 
 /**
@@ -18,8 +21,16 @@ import javax.swing.JTextField;
  *
  */
 public class SinglePageApplication {
-	
+
 	private static final int MAX_NUMBER_VALUE = 1000;
+	private static final int BORDER_VALUE = 31;
+
+	private static JFrame jFrame = getFrame();
+	private static JPanel jPanel = new JPanel();
+
+	private SinglePageApplication() {
+
+	}
 
 	public static void main(String[] args) {
 
@@ -29,48 +40,192 @@ public class SinglePageApplication {
 
 	private static void createSwingApp() {
 
-		JFrame jFrame = getFrame();
-		JPanel jPanel = new JPanel();
+		jPanel.setBackground(Color.WHITE);
 		jFrame.add(jPanel);
-	
-		JLabel jLabel = new JLabel("How many numbers to display?");
+		generateIntroPagePanel();
+
+	}
+
+	private static void generateIntroPagePanel() {
+
+		final JLabel jLabel = new JLabel("How many numbers to display?");
+		jLabel.setHorizontalAlignment(JLabel.CENTER);
 		jPanel.add(jLabel);
-		
-		JTextField jTextField = new JTextField(20);
+
+		final JTextField jTextField = new JTextField(15);
 		jPanel.add(jTextField);
 
-		JButton jButton = new JButton("Enter");
-//		jButton.setBackground(Color.MAGENTA);
-//		jButton.setOpaque(true);
-//		jButton.setBorderPainted(false);
-		jButton.addActionListener(new ActionListener() {
+		final JButton enterButton = new JButton("Enter");
+		enterButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-//				jPanel.setBackground(Color.BLUE);
-				System.out.println(jTextField.getText());
+
+				jPanel.removeAll();
+				jPanel.updateUI();
+				String enteredString = jTextField.getText();
+				int enteredAmountOfNumbers = 0;
+				try {
+					enteredAmountOfNumbers = Integer.parseInt(enteredString);
+				} catch (Exception exception) {
+
+				}
+				generateSortPagePanel(enteredAmountOfNumbers);
+				jPanel.repaint();
+
 			}
 		});
-		jPanel.add(jButton);
-		
-		JPopupMenu jPopupMenu = new JPopupMenu();
-		jPopupMenu.add(new JMenuItem("test"));
-		jPanel.setComponentPopupMenu(jPopupMenu);
-		
+		jPanel.add(enterButton);
+
+//		JPopupMenu jPopupMenu = new JPopupMenu();
+//		jPopupMenu.add(new JMenuItem("test"));
+//		jPanel.setComponentPopupMenu(jPopupMenu);
+
 		jPanel.revalidate();
 
 //		JScrollBar jScrollBar = new JScrollBar();
 //		jPanel.add(jScrollBar);
 
+	}
+
+	private static void generateSortPagePanel(int amountOfNumbers) {
+
+//		jPanel.setLayout(new BorderLayout());
+
+		final Random random = new Random();
+		List<Integer> numbers = new ArrayList<Integer>();
+		final int indexOfNumberLessThanBorderValue = random.nextInt(amountOfNumbers);
+		for (int i = 0; i < amountOfNumbers; i++) {
+			if (i == indexOfNumberLessThanBorderValue) {
+				numbers.add(generateRandomNumberButton(BORDER_VALUE));
+			} else {
+				numbers.add(generateRandomNumberButton(MAX_NUMBER_VALUE));
+			}
+		}
+
+		final JButton sortButton = new JButton("Sort");
+		sortButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				jPanel.removeAll();
+				jPanel.updateUI();
+//				Collections.sort(numbers);
+//				Collections.reverse(numbers);
+//				quickSort(
+				sortNumbersOnSortPagePanel(numbers);
+				jPanel.repaint();
+			}
+		});
+		jPanel.add(sortButton);
+
+		final JButton resetButton = new JButton("Reset");
+		resetButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				jPanel.removeAll();
+				jPanel.updateUI();
+				generateIntroPagePanel();
+				jPanel.repaint();
+			}
+		});
+		jPanel.add(resetButton);
+
+		jPanel.revalidate();
 
 	}
+	
+	private static void sortNumbersOnSortPagePanel(List<Integer> numberButtons) {
+		
+		for (Integer number : numberButtons) {
+			createNumberButton(number);
+		}
+
+		final JButton sortButton = new JButton("Sort");
+		sortButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				jPanel.removeAll();
+				jPanel.updateUI();
+				Collections.reverse(numberButtons);
+				sortNumbersOnSortPagePanel(numberButtons);
+				jPanel.repaint();
+			}
+		});
+		jPanel.add(sortButton);
+
+		final JButton resetButton = new JButton("Reset");
+		resetButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				jPanel.removeAll();
+				jPanel.updateUI();
+				generateIntroPagePanel();
+				jPanel.repaint();
+			}
+		});
+		jPanel.add(resetButton);
+
+		jPanel.revalidate();
+		
+	}
+	
+	private static int generateRandomNumberButton(int randomSpreading) {
+		
+		final Random random = new Random();
+		final int randomNumber = random.nextInt(randomSpreading);
+		final JButton numberButton = new JButton(String.valueOf(randomNumber));
+		numberButton.setBackground(new Color(200, 200, 200));
+		jPanel.add(numberButton);
+		return randomNumber;
+		
+	}
+	
+	private static void createNumberButton(int number) {
+		
+		final JButton numberButton = new JButton(String.valueOf(number));
+		numberButton.setBackground(new Color(200, 200, 200));
+		jPanel.add(numberButton);
+		
+	}
+	
+//	public static void quickSort(int[] array) {
+//        int startIndex = 0;
+//        int endIndex = ARRAY_LENGTH - 1;
+//        doSort(startIndex, endIndex);
+//    }
+//
+//    private static void doSort(int start, int end) {
+//        if (start >= end)
+//            return;
+//        int i = start, j = end;
+//        int cur = i - (i - j) / 2;
+//        while (i < j) {
+//            while (i < cur && (array[i] <= array[cur])) {
+//                i++;
+//            }
+//            while (j > cur && (array[cur] <= array[j])) {
+//                j--;
+//            }
+//            if (i < j) {
+//                int temp = array[i];
+//                array[i] = array[j];
+//                array[j] = temp;
+//                if (i == cur)
+//                    cur = j;
+//                else if (j == cur)
+//                    cur = i;
+//            }
+//        }
+//        doSort(start, cur);
+//        doSort(cur+1, end);
+//    }
 
 	private static JFrame getFrame() {
 
-		JFrame jFrame = new JFrame();
+		final JFrame jFrame = new JFrame();
 		jFrame.setVisible(true);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		Dimension dimension = toolkit.getScreenSize();
+		final Toolkit toolkit = Toolkit.getDefaultToolkit();
+		final Dimension dimension = toolkit.getScreenSize();
 		jFrame.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 150, 500, 300);
 		jFrame.setTitle("Single Page Application");
 		return jFrame;
